636ff62b5346b506dd0026b72b3722a1db753084
arshedgithub <arshedahmedmail@gmail.com>
2024-05-07 11:20:34 +0530

    Implementing put post delete methods for item

--- 3_ServerApp/src/main/java/lk/earth/earthuniversity/dao/ItemDao.java
+++ 3_ServerApp/src/main/java/lk/earth/earthuniversity/dao/ItemDao.java
@@ -3,7 +3,18 @@ package lk.earth.earthuniversity.dao;
 
 import lk.earth.earthuniversity.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ItemDao extends JpaRepository<Item,Integer> {
+
+    @Query("select i from Item i where i.code=:code ")
+    Item findByCode(@Param("code")String code);
+
+    @Query("select i from Item i where i.name=:name")
+    Item findByName(@Param("name")String name);
+
+    @Query("select i from Item i where i.id=:id")
+    Item findByMyId(@Param("id")Integer id);
 
 }
