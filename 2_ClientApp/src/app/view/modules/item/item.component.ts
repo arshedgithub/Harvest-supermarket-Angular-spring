f74640b2b91af995b940a04ce1f2ebb333978876
arshedgithub <arshedahmedmail@gmail.com>
2024-05-30 04:10:43 +0530

    add client search

--- 2_ClientApp/src/app/view/modules/item/item.component.ts
+++ 2_ClientApp/src/app/view/modules/item/item.component.ts
@@ -1,5 +1,5 @@
 import {Component, ViewChild} from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 import {MatTableDataSource} from "@angular/material/table";
 import {MatPaginator} from "@angular/material/paginator";
 import {Item} from "../../../entity/item";
@@ -18,7 +18,7 @@ export class ItemComponent {
   headers: string[] = ['Name', 'Code', 'Unit Type', 'Sale Price', 'Purchase Price', 'Modification'];
   binders: string[] = ['name', 'code', 'unittype.name', 'sprice', 'pprice', 'getModi()'];
 
-  cscolumns: string[] = ['csnumber', 'cscallingname', 'csgender', 'csdesignation', 'csname', 'csmodi'];
+  cscolumns: string[] = ['csname', 'cscode', 'csunittype', 'cssprice', 'cspprice', 'csmodi'];
   csprompts: string[] = ['Search by Number', 'Search by Name', 'Search by Gender',
     'Search by Designation', 'Search by Full Name', 'Search by Modi'];
 
@@ -35,6 +35,15 @@ export class ItemComponent {
   constructor(private fb:FormBuilder, private is:ItemService) {
 
     this.uiassist = new UiAssist(this);
+
+    this.csearch = this.fb.group({
+      'csname': new FormControl(),
+      'cscode': new FormControl(),
+      'csunittype': new FormControl(),
+      'cssprice': new FormControl(),
+      'cspprice': new FormControl(),
+      'csmodi': new FormControl()
+    })
 
   }
 
@@ -66,8 +75,24 @@ export class ItemComponent {
         this.data = new MatTableDataSource(this.items);
         this.data.paginator = this.paginator;
       });
+  }
+
+  filterTable():void {
+    const cssearchdata = this.csearch.getRawValue();
+
+    this.data.filterPredicate = ((item: Item, filter: String) => {
+      return (cssearchdata.csname == null || item.name.includes(cssearchdata.csname)) &&
+        (cssearchdata.cscode == null || item.code.includes(cssearchdata.cscode)) &&
+        (cssearchdata.csunittype == null || item.unittype.name.includes(cssearchdata.csunittype)) &&
+        (cssearchdata.cssprice == null || item.sprice.toString().includes(cssearchdata.cssprice)) &&
+        (cssearchdata.cspprice == null || item.pprice.toString().includes(cssearchdata.cspprice)) &&
+        (cssearchdata.csmodi == null || this.getModi(item).toLowerCase().includes(cssearchdata.csmodi));
+    });
+    this.data.filter = "xx";
 
   }
+
+
 
   getModi(element: Item) {
     return element.name + '(' + element.code + ')';
